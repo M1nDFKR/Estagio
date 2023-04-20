47712a2c2a39774b8b28dcc7b6020a6b9340cf84
M1nDFKR <tomasffjesus12@gmail.com>
2023-04-20 16:42:45 +0100

    Make migrations

--- TicketsSPA/tickets/views.py
+++ TicketsSPA/tickets/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home') # replace "home" with the name of your homepage URL
+        else:
+            error_message = "Invalid login credentials."
+    else:
+        error_message = ""
+    return render(request, 'login.html', {'error_message': error_message})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home') # replace "home" with the name of your homepage URL
+
