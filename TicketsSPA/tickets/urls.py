2661d48ba8f81dd52db612c9009c8d51ba8da027
M1nDFKR <tomasffjesus12@gmail.com>
2023-06-29 14:55:02 +0100

    Tickets Color test2/ ellipses when exceeds the title box

--- TicketsSPA/tickets/urls.py
+++ TicketsSPA/tickets/urls.py
@@ -7,11 +7,13 @@ urlpatterns = [
     path('', auth_views.LoginView.as_view(
         template_name='login.html'), name='login'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/login/',
+         auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('home/', views.HomeView.as_view(template_name='home.html'), name='home'),
     path('myadmin/', admin_site.urls),
     path('add_comment/<int:ticket_id>/', views.add_comment, name='add_comment'),
-    path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
+    path('delete_comment/<int:comment_id>/',
+         views.delete_comment, name='delete_comment'),
     path('logout_on_close/', views.logout_on_close, name='logout_on_close'),
 ]
